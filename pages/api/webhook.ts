8a8c3ebed4901cb6e7fde1c83c7db9370b9031de
Adam Sobotka <vorcigernix@gmail.com>
2023-02-20 18:11:16 +0100

    ISR (maybe)

---
+++ pages/api/webhook.ts
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  if (!isValidRequest(req)) {
+    return res.status(401).json({ message: 'Invalid request' })
+  }
+
+  try {
+    const pathToRevalidate = getPathToRevalidate(req)
+    if (!pathToRevalidate) res.status(400).json({ message: 'missing article' })
+    await res.revalidate(pathToRevalidate)
+    return res.json({ revalidated: true })
+  } catch (err) {
+    return res.status(500).json({ message: 'Error revalidating: ' + err })
+  }
+}
+
+const isValidRequest = (req) => {
+  const signature = req.headers['x-blocken-signature']
+  return signature == process.env.BLOCKEN_TOKEN
+}
+const getPathToRevalidate = (req) => {
+  if (!req.body.article) return
+  return `/posts/${req.body.article}`
+}
