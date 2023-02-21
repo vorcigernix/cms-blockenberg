c2bb6f3f3f4e176077a3e5134e827986c5709b04
Adam Sobotka <vorcigernix@gmail.com>
2023-02-21 11:38:19 +0100

    cross

--- next.config.js
+++ next.config.js
@@ -10,4 +10,18 @@ module.exports = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/webhook",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "OPTIONS,POST" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-blocken-signature" },
+        ]
+      }
+    ]
+  }
 }
